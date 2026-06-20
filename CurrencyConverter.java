4eb42ccf448d1a0a2d0ab37eb5b7977ac5addc4c
norahali6 <norahali3425@outlook.sa>
2026-06-20 16:53:10 +0300

    Create CurrencyConverter.java

---
+++ CurrencyConverter.java
@@ -0,0 +1,8 @@
+public class CurrencyConverter {
+    public static void main(String[] args) {
+        double riyal = 100;
+        double usd = riyal/3.75;
+      System.out.println(riyal+"SAR is equal to"+usd+"USD");
+    }
+}
+        
